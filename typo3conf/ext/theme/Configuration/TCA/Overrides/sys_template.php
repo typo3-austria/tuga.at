5b55676401b0edb4c0147c7e4117d742c1306c2d
Josef Glatz <jousch@gmail.com>
2015-11-03 11:02:10 +0100

    [FEATURE] theme: addStaticFile (TypoScript)

---
+++ typo3conf/ext/theme/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,23 @@
+<?php
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3_MODE') || die ('Access denied.');
+
+/*
+Remove this file if you include the TypoScript in the template fields manually.
+Use this syntax for inclusion:
+
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:theme/Configuration/TypoScript/Constants/" extensions="ts">
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:theme/Configuration/TypoScript/Setup/" extensions="ts">
+
+*/
+
+// Embed static TypoScript template(s)
+$websites = array('Base', 'Tree01');
+foreach($websites as $website) {
+    ExtensionManagementUtility::addStaticFile(
+        'theme',
+        'Configuration/TypoScript/Tree/' . $website,
+        'Thm TS:' . $website
+    );
+}
