8e8fa78e18058a2da1f483bb2434adfcbc2cc660
Josef Glatz <jousch@gmail.com>
2015-11-03 10:37:45 +0100

    [TASK] theme: add basic ext_emconf

---
+++ typo3conf/ext/theme/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Theme by Sup7even Digital',
+    'description' => '',
+    'category' => 'Theme',
+    'shy' => 0,
+    'version' => '0.0.0',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 1,
+    'lockType' => '',
+    'author' => 'Sup7even Development',
+    'author_email' => 'office@supseven.at',
+    'author_company' => 'sup7even.digital',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            // todo clear dependencies
+            'typo3' => '7.5.0-7.6.99',
+            'news' => '3.1.0',
+            'vhs' => '',
+            'formhandler' => '',
+            'realurl' => '',
+            'ccontent' => '',
+            'static_info_tables' => '',
+            'gridelements' => '',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);
