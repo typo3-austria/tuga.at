f201563df7bca5071a256cbe12c30e636686b516
Josef Glatz <jousch@gmail.com>
2015-11-05 11:23:28 +0100

    [FEATURE] theme: doktype "external url" > activate realurl page properties

--- typo3conf/ext/theme/Configuration/TCA/Overrides/pages.php
+++ typo3conf/ext/theme/Configuration/TCA/Overrides/pages.php
@@ -7,3 +7,13 @@ defined('TYPO3_MODE') || die('Access denied.');
     'Configuration/TSConfig/PageGeneral.tsc',
     'EXT:theme :: General PageTSConfig'
 );
+
+// Adds ext:realurl tca palette to pagetype "Link to External URL"
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl', true)) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        'tx_realurl_pathsegment;;137;;,tx_realurl_exclude',
+        '3',
+        'after:nav_title'
+    );
+}
