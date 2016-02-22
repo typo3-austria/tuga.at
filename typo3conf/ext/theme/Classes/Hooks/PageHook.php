375566a9fc33fffebbfbc9dcb7bda913d4afeb11
Josef Glatz <jousch@gmail.com>
2016-02-22 21:52:46 +0100

    [TASK] Finalize page module hook

--- typo3conf/ext/theme/Classes/Hooks/PageHook.php
+++ typo3conf/ext/theme/Classes/Hooks/PageHook.php
@@ -15,7 +15,9 @@ namespace SUP7\Theme\Hooks;
  */
 use TYPO3\CMS\Backend\Controller\PageLayoutController;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Backend\View\BackendLayoutView;
 use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Hook for the page module
@@ -27,6 +29,7 @@ class PageHook
 
     /**
      * Add additional content to the footer of the page module
+     * when the actual page is a meetup
      *
      * @param array $params
      * @param PageLayoutController $parentObject
@@ -35,21 +38,24 @@ class PageHook
     public function render(array $params = array(), PageLayoutController $parentObject)
     {
 
-        if ($parentObject->pageinfo['meetup_time'] || $parentObject->pageinfo['meetup_sponsor'] || $parentObject->pageinfo['meetup_link']) {
-            $NecessaryFieldsFilled = 'f2dede';
+        $rawBackendLayout = GeneralUtility::callUserFunction(BackendLayoutView::class . '->getSelectedCombinedIdentifier', $parentObject->pageinfo['uid'], $this);
+        $backendLayout = preg_replace("/.*__/", "", $rawBackendLayout);
 
-            if ($parentObject->pageinfo['meetup_time'] && $parentObject->pageinfo['meetup_sponsor'] && $parentObject->pageinfo['meetup_link']) {
-                $NecessaryFieldsFilled = 'dff0d8';
+        if ($backendLayout == 'Default') {
+            if ($parentObject->pageinfo['meetup_time'] || $parentObject->pageinfo['meetup_sponsor'] || $parentObject->pageinfo['meetup_link']) {
+                $NecessaryFieldsFilled = 'f2dede';
+
+                if ($parentObject->pageinfo['meetup_time'] && $parentObject->pageinfo['meetup_sponsor'] && $parentObject->pageinfo['meetup_link']) {
+                    $NecessaryFieldsFilled = 'dff0d8';
+                }
+                $footerContent = '<div class="panel panel-default" style="padding: 20px; background-color:#' . $NecessaryFieldsFilled . '">';
+                $footerContent .= '    <h4>Meetup Infos</h4>';
+                $footerContent .= '    <p><b>Meetup Date/Time:</b> ' . BackendUtility::datetime($parentObject->pageinfo['meetup_time']) . '</p>';
+                $footerContent .= '    <p><b>Meetup Link:</b> ' . $parentObject->pageinfo['meetup_link'] . '</p>';
+                $footerContent .= '    <p><b>Meetup Location Sponsor:</b> ' . $parentObject->pageinfo['meetup_sponsor'] . '</p>';
+
+                return $footerContent .= '</div>';
             }
-            $footerContent = '<div class="panel panel-default" style="padding: 20px; background-color:#' . $NecessaryFieldsFilled . '">';
-            $footerContent .= '    <h4>Page Infos</h4>';
-            $footerContent .= '    <p><b>Meetup Date/Time:</b> ' . BackendUtility::datetime($parentObject->pageinfo['meetup_time']) . '</p>';
-            $footerContent .= '    <p><b>Meetup Link:</b> ' . $parentObject->pageinfo['meetup_link'] . '</p>';
-            $footerContent .= '    <p><b>Meetup Location Sponsor:</b> ' . $parentObject->pageinfo['meetup_sponsor'] . '</p>';
-
-            return $footerContent .= '</div>';
-
-
         }
     }
 
